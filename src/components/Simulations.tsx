fa31d8f21c0010b88589d9e2f491bb72c7b05aa3
Alejandro <alejandro_portaluppi@outlook.com>
2023-10-21 21:40:31 -0300

    Aclaración

--- src/components/Simulations.tsx
+++ src/components/Simulations.tsx
@@ -4,8 +4,9 @@ import Simulation from "./Simulation.js"
 const Simulations = () => {
     return (
         <>
-        <div>
+        <div className="px-2">
             <p className="mt-1">Bienvenido! Utiliza las simulaciones que más te convenga. Puedes consultar la TNA <a className="text-blue-800 font-semibold" href="https://www.bcra.gob.ar/BCRAyVos/Plazos_fijos_online.asp" target="_blank" rel="noreferrer noopener">aquí</a>. Si tienes alguna duda puedes leer el <a className="text-blue-800 font-semibold" href="https://github.com/Ale6100/simulador-plazo-fijo.git#simulaciones" target="_blank" rel="noreferrer noopener">readme</a> en el código fuente o la teoría que se encuentra al final.</p>
+            <p>Considera que la herramienta que uso para graficar fuerza a que los datos del eje horizontal estén equiespaciados</p>
         </div>
 
         {
